9cdea4c09186221b6b503139078d42b081d253b7
itsDario <itsDario1@gmail.com>
2014-11-04 10:17:21 -0500

    copied in controls

---
+++ ShibuControls/Assets/Movement1.js
@@ -0,0 +1,154 @@
+﻿#pragma strict
+
+var speed: float;
+var state: String;
+var dashTime: float;
+var onFloor: boolean;
+var onWall: boolean;
+var onRightWall: boolean;
+var onLeftWall: boolean;
+private var hDash: boolean;
+private var dDash: boolean;
+private var jumping: boolean;
+private var wallTime: float;
+var jumpTime: float = 0;
+var maxJumpValue:float = 100;
+var jumpPower: float;
+var jumpSustain: float;
+var direction: int;
+
+var rightStart: Transform;
+var rightEnd: Transform;
+
+var leftStart: Transform;
+var leftEnd: Transform;
+
+public class Movement1 extends TouchScript
+{
+function Start () {
+	gravity(true);
+	direction = 1;
+}
+
+function Update () {
+rigidbody2D.velocity.x = speed * Time.deltaTime;
+//Debug.Log(jumpTime);
+
+keyBoardInput();
+horizontalDashing();
+downDashing();
+isJumping();
+wallCheck();
+}
+
+function movement(){
+//	switch(getMovement()){
+//		case up:
+//		break;
+//	}
+}
+
+function horizontalDashing(){
+	if(hDash){
+		gravity(false);
+		//set velocity		
+		rigidbody2D.velocity.x = (speed * 2) * Time.deltaTime * direction;
+		rigidbody2D.velocity.y = 0;
+		//yield
+		yield WaitForSeconds(dashTime);
+		//turn gravity back on
+		gravity(true);
+		//dash = false
+		hDash = false;
+		direction = 1;
+	}
+}
+
+function downDashing(){
+	if(dDash){
+		gravity(false);
+		//set velocity		
+		rigidbody2D.velocity.y = (-speed * 2) * Time.deltaTime;
+		rigidbody2D.velocity.x = 0;
+		//yield
+		yield WaitForSeconds(dashTime);
+		//turn gravity back on
+		gravity(true);
+		//dash = false
+		dDash = false;
+	}
+}
+
+function isJumping(){
+	if (Input.GetKeyDown ("up") && onFloor) {
+		rigidbody2D.AddForce(Vector2.up * jumpPower,ForceMode2D.Impulse);
+	}  
+     if (Input.GetKey ("up")){
+//     Debug.Log("extraForce");
+        if (jumpTime < maxJumpValue) {
+          rigidbody2D.AddForce(Vector2.up * jumpSustain,ForceMode2D.Force);
+          jumpTime += Time.deltaTime;
+        }
+	}
+}
+
+function wallCheck(){
+	Debug.DrawLine(rightStart.position,rightEnd.position,Color.green);
+	onRightWall = Physics2D.Linecast(rightStart.position,rightEnd.position);
+	
+	Debug.DrawLine(leftStart.position,leftEnd.position,Color.green);
+	onLeftWall = Physics2D.Linecast(leftStart.position,leftEnd.position);
+}
+
+function wallHang(){
+		onWall = true;
+		hDash = false;
+		gravity(false);
+		rigidbody2D.velocity.y = 0;
+		yield WaitForSeconds(2);
+		gravity(true);
+}
+
+function keyBoardInput(){
+	//down = slam
+	if (Input.GetKeyDown ("down") && !onFloor){
+		dDash = true;
+	}
+	//right = dash
+	if (Input.GetKeyDown ("right") && !onRightWall){
+		hDash = true;
+		direction = 1;
+	}
+//	left = dash
+//	if (Input.GetKeyDown ("left")){
+//		hDash = true;
+//		direction = -1;
+//	}
+}
+
+function gravity(gravity : boolean){
+	if(gravity){
+		rigidbody2D.gravityScale = 1;
+	}else{
+		rigidbody2D.gravityScale = 0;
+	}
+}
+
+function OnCollisionEnter2D (other : Collision2D) {
+dDash = false;
+	if(other.gameObject.tag == "floor"){
+		jumpTime = 0;
+		onFloor = true;
+	}
+	else if (other.gameObject.tag == "wall" && (onLeftWall || onRightWall)){
+		jumpTime = 0;
+		wallTime = Time.deltaTime;
+		wallHang();
+	}
+}
+
+function OnCollisionExit2D () {
+	onFloor = false;
+	onWall = false;
+}
+}
---
+++ ShibuControls/Assets/TouchScript.js
@@ -0,0 +1,72 @@
+﻿#pragma strict
+
+var swipeDirection: String;
+
+var minSwipeDistY : float;
+
+var minSwipeDistX : float;
+
+var Swipe: GUIText;
+
+private var startPos :Vector2; 
+
+function Update () {
+
+	if (Input.touchCount > 0) {
+
+	var touch : Touch = Input.touches[0];
+
+	switch (touch.phase){
+
+		case TouchPhase.Began:
+		startPos = touch.position;
+		break;
+
+
+			case TouchPhase.Ended:
+			var swipeDistVertical : float;
+			swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
+
+				if (swipeDistVertical > minSwipeDistY) 
+				{
+					var swipeValue : float;
+					swipeValue = Mathf.Sign(touch.position.y - startPos.y);
+
+					if (swipeValue > 0)//up
+					{	
+						setDirection("up");
+					}
+					else if (swipeValue < 0)//down
+					{	
+						setDirection("down");
+					}
+				}
+				var swipeDistHorizontal : float;
+
+				swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+
+				if (swipeDistHorizontal > minSwipeDistX) 
+				{
+					swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+
+					if (swipeValue > 0)//right
+					{
+						//MoveRight ();
+						setDirection("right");
+					}
+					else if (swipeValue < 0)//left
+					{	
+						//MoveLeft ();
+						setDirection("left");
+					}
+				}
+				break;
+			}
+		}	
+	}
+	function setDirection(newDirection: String){
+		swipeDirection = newDirection;
+	}
+	function getDirection(){
+		return swipeDirection;
+	}
